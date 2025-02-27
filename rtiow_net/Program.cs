63b54985ae4bccdfe1cc23ccba839c9fedfa181c
Gerald Stevens <gerbob@gmail.com>
2025-02-26 20:22:42 -0500

    basic impl for .net and rust

--- rtiow_net/Program.cs
+++ rtiow_net/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int imageWidth = 256;
+int imageHeight = 256;
+
+Console.WriteLine("P3");
+Console.WriteLine($"{imageWidth} {imageHeight}");
+Console.WriteLine("255");
+
+foreach (var j in Enumerable.Range(0, imageHeight))
+{
+    foreach (var i in Enumerable.Range(0, imageWidth))
+    {
+        var r = (double)i / (imageWidth - 1);
+        var g = (double)j / (imageHeight - 1);
+        var b = 0.5 * ( i + j);
+
+        var ir = (int)(255.999 * r);
+        var ig = (int)(255.999 * g);
+        var ib = (int)(255.999 * b);
+
+        Console.WriteLine($"{ir} {ig} {ib}");
+    }
+}
--- rtiow_rs/src/main.rs
+++ rtiow_rs/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = f64::from(i) / f64::from(image_width - 1);
+            let g = f64::from(j) / f64::from(image_height -1);
+            let b = 0.5 * f64::from(i + j);
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
